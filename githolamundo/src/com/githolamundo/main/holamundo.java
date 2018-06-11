9a5cc78f63aeb88bb1a1e47b5fbd5accb13a8ced
rjop95 <riccardo.jop@gmail.com>
2018-06-11 16:47:43 -0500

    segunda version de mi aplicacion, segundo hola mundo

--- githolamundo/src/com/githolamundo/main/holamundo.java
+++ githolamundo/src/com/githolamundo/main/holamundo.java
@@ -4,7 +4,8 @@ public class holamundo {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-
+		System.out.println("hola mundo2");
+		System.out.println("hola mundo3");
 	}
 
 }
